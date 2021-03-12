c24006b23797cf4240f142190e6c3e59bbdf2168
Hans Olav Sund <hans.olav.sund@digdir.no>
2021-03-12 09:26:54 +0100

    chore: fixed fail file creation

--- files/transform_datasets.py
+++ files/transform_datasets.py
@@ -9,21 +9,21 @@ args = parser.parse_args()
 update_dates = os.environ["TO_BE_UPDATED"] == 'dates'
 
 
-def transform(inputfile, inputfile_meta):
+def transform(inputfile, inputfile_meta, failed_path):
 
     datasets = openfile(inputfile)
     datasets_meta = openfile(inputfile_meta)
 
     transformed = {}
     failed = {}
-    failed_transform = args.outputdirectory + "failed_transform.json"
+
     for dataset_key in datasets:
         if dataset_key not in datasets_meta:
             failed_transform.append(datasets[dataset_key]["_id"])
         else:
             transformed[datasets[dataset_key]["_id"]] = fields_to_change(datasets[dataset_key])
 
-    with open(failed_transform, 'w', encoding="utf-8") as failed_file:
+    with open(failed_path, 'w', encoding="utf-8") as failed_file:
         json.dump(failed, failed_file, ensure_ascii=False, indent=4)
     return transformed
 
@@ -44,12 +44,14 @@ def fields_to_change(dataset):
 inputfileName = args.outputdirectory + "mongo_datasets.json"
 inputfileNameMeta = args.outputdirectory + "mongo_datasetsMeta.json"
 outputfileName = args.outputdirectory + "datasets_transformed.json"
+failedfileName = args.outputdirectory + "failed_transform_datasets.json"
 with open(outputfileName, 'w', encoding="utf-8") as outfile:
-    json.dump(transform(inputfileName, inputfileNameMeta), outfile, ensure_ascii=False, indent=4)
+    json.dump(transform(inputfileName, inputfileNameMeta, failedfileName), outfile, ensure_ascii=False, indent=4)
 
 # Catalogs
 inputfileName = args.outputdirectory + "mongo_catalogs.json"
 inputfileNameMeta = args.outputdirectory + "mongo_catalogsMeta.json"
 outputfileName = args.outputdirectory + "catalogs_transformed.json"
+failedfileName = args.outputdirectory + "failed_transform_catalogs.json"
 with open(outputfileName, 'w', encoding="utf-8") as outfile:
-    json.dump(transform(inputfileName, inputfileNameMeta), outfile, ensure_ascii=False, indent=4)
+    json.dump(transform(inputfileName, inputfileNameMeta, failedfileName), outfile, ensure_ascii=False, indent=4)
