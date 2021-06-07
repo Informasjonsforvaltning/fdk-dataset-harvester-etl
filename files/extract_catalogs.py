7543d5c645051c5f07a60e6ce76338b0e6d0c118
Hans Olav Sund <hans.olav.sund@digdir.no>
2021-06-07 15:06:40 +0200

    chore: ETL for catalogMeta ids

---
+++ files/extract_catalogs.py
@@ -0,0 +1,22 @@
+import json
+import os
+from pymongo import MongoClient
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-o', '--outputdirectory', help="the path to the directory of the output files", required=True)
+args = parser.parse_args()
+connection = MongoClient(
+    f"""mongodb://{os.environ['MONGO_USERNAME']}:{os.environ['MONGO_PASSWORD']}@mongodb:27017/datasetCatalog?authSource=admin&authMechanism=SCRAM-SHA-1""")
+
+db = connection.datasetCatalog
+dict_list = list(db.catalogs.find())
+catalogs = {}
+for id_dict in dict_list:
+    id_str = id_dict["_id"]
+    catalogs[id_str] = {}
+    catalogs[id_str]["uri"] = id_dict.get("uri")
+print("Total number of extracted catalogs: " + str(len(catalogs)))
+
+with open(args.outputdirectory + 'mongo_catalogs.json', 'w', encoding="utf-8") as outfile:
+    json.dump(catalogs, outfile, ensure_ascii=False, indent=4)
---
+++ files/load_catalogs.py
@@ -0,0 +1,20 @@
+import json
+import os
+from pymongo import MongoClient
+import argparse
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-o', '--outputdirectory', help="the path to the directory of the output files", required=True)
+args = parser.parse_args()
+connection = MongoClient(
+    f"""mongodb://{os.environ['MONGO_USERNAME']}:{os.environ['MONGO_PASSWORD']}@mongodb:27017/datasetHarvester?authSource=admin&authMechanism=SCRAM-SHA-1""")
+db = connection.datasetHarvester
+
+with open(args.outputdirectory + 'catalogs_transformed.json') as catalogs_file:
+    transformed_json = json.load(catalogs_file)
+
+    for mongo_id in transformed_json:
+        to_be_updated = transformed_json[mongo_id]
+        print(db.catalogMeta.find_one_and_update({'_id': mongo_id},  {'$set': to_be_updated}))
+
