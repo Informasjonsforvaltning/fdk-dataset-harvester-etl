5fba679dbb93fe884983c9c9e0ee805006adc3be
Hans Olav Sund <hans.olav.sund@digdir.no>
2021-06-08 09:44:32 +0200

    chore: ETL for catalogMeta ids

--- files/load_catalogs.py
+++ files/load_catalogs.py
@@ -19,5 +19,9 @@ with open(args.outputdirectory + 'catalogs_transformed.json') as catalogs_file:
         if mongo_id == to_be_updated.get("_id"):
             print("Not updating identical ids: " + mongo_id)
         else:
-            print(db.catalogMeta.insert_one(to_be_updated))
-            print(db.catalogMeta.delete_one(mongo_id))
+            print("Inserting ID: " + to_be_updated.get("_id"))
+            insert_result = db.catalogMeta.insert_one(to_be_updated)
+            print("Result ID: " + insert_result.inserted_id)
+            print("Deleting ID: " + mongo_id)
+            delete_result = db.catalogMeta.delete_one({"_id": mongo_id})
+            print("Documents deleted: " + delete_result.deleted_count)
